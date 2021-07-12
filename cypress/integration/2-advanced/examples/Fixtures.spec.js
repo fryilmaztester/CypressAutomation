b4ee0bfb9860b68f6cda9132013836d87231c11d
Fatih YILMAZ <fatih.yilmaz@foreks.com>
2021-07-12 11:51:47 +0300

    Fixtures

---
+++ cypress/integration/2-advanced/examples/Fixtures.spec.js
@@ -0,0 +1,45 @@
+/// <reference types = "cypress" />
+
+/**
+ 
+Load a fixed set of data loacated in a file
+
+Doc: https://docs.cypress.io/api/commands/fixture#Syntax 
+
+ */
+
+var userNameLocator = "input[name='userName']";
+var passWordLocator = "input[name='password']";
+var submitButton = "input[value='Submit']";
+
+
+var urlStr = "http://demo.guru99.com/test/newtours/";
+
+describe('FixtureSuite Test' , () => {
+
+    
+    before(function () {
+        // "this" points at the test context object
+        cy.fixture('user').then((user) => {
+          // "this" is still the test context object
+          this.user = user
+        })
+      })
+      
+
+    it('Fixtures Test',  () => {
+
+        cy.visit(urlStr); // Go to the url
+
+        cy.get(userNameLocator).type(user.user.userName); // userName
+
+        cy.get(passWordLocator).type(user.user.passWord); // passWord
+
+        cy.get(submitButton).click(); // submitButton
+
+
+    })
+
+
+
+})
